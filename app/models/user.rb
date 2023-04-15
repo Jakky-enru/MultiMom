b113b1b400e17b70ad2ad83b3ac2a95bf36e610a
下津浦良 <shimotsuuraryo@shitatsuuraryounoMacBook-Air.local>
2023-04-16 00:10:08 +0900

    devise, rails admin, cancancan実装

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { parent: 0, child: 1, admin: 2 }, _prefix: true
+
+end
