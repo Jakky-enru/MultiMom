8d871d6a19e9e1ad37d1cd20fd733a9be214b5cf
下津浦良 <shimotsuuraryo@shitatsuuraryounoMacBook-Air.local>
2023-04-18 17:21:23 +0900

    お気に入り機能実装、各アソシエーション定義

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,13 @@
+class FavoritesController < ApplicationController
+  def create
+    # @blog = Blog.find(params[:blog_id])
+    favorite_comment = current_user.favorites.create!(user_id: current_user.id, comment_id: params[:comment_id])
+    redirect_to blog_path(params[:blog_id])
+  end
+
+  def destroy
+    blog_id = Comment.find(Favorite.find(params[:id]).comment_id).blog_id
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to blog_path(blog_id)
+  end
+end
