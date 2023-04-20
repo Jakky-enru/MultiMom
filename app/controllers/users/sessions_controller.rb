22726b479f7fe4f35c243ba438abbafb54e74230
下津浦良 <shimotsuuraryo@shitatsuuraryounoMacBook-Air.local>
2023-04-21 00:15:18 +0900

    ゲストログイン機能実装

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+
+  def guest_parent_sign_in
+    user = User.guest_parent
+    sign_in User.guest_parent
+    redirect_to blogs_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  def guest_child_sign_in
+    user = User.guest_child
+    sign_in User.guest_child
+    redirect_to blogs_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  def guest_admin_sign_in
+    user = User.guest_admin
+    sign_in User.guest_admin
+    redirect_to blogs_path, notice: 'ゲスト管理者としてログインしました。'
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,33 @@ class User < ApplicationRecord
   has_many :blogs
   has_many :comments
   has_many :favorites, dependent: :destroy
+  validates :role, presence: true
+
+  validates :name, presence: true, length: { maximum: 300 }
+  validates :email, presence: true, length: { maximum: 300 }
+  validates :password, length: { minimum: 6, maximum: 300 }, if: :password_required?, allow_blank: true
+
+  def self.guest_parent
+    find_or_create_by!(email: 'guest_parent@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー(親)"
+      user.role = "parent"
+    end
+  end
+
+  def self.guest_child
+    find_or_create_by!(email: 'guest_child@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー(子)"
+      user.role = "child"
+    end
+  end
+
+  def self.guest_admin
+    find_or_create_by!(email: 'guest_admin@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー(管理者)"
+      user.role = "admin"
+    end
+  end
 end
