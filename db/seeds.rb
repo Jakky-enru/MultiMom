bde102afb8a30341e544d2285d3fe8f108f74fdb
下津浦良 <shimotsuuraryo@shitatsuuraryounoMacBook-Air.local>
2023-04-24 14:28:16 +0900

    seedデータ導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+6.times do |n|
+  User.create!(
+    email: "parent#{n + 1}@gmail.com",
+    name: "親#{n + 1}",
+    password: "password#{n + 1}",
+    role: :parent
+  )
+end
+
+6.times do |n|
+  User.create!(
+    email: "child#{n + 1}@gmail.com",
+    name: "子#{n + 1}",
+    password: "password#{n + 1}",
+    role: :child
+  )
+end
+
+6.times do |n|
+  User.create!(
+    email: "admin#{n + 1}@gmail.com",
+    name: "管理者#{n + 1}",
+    password: "password#{n + 1}",
+    role: :admin
+  )
+end
+
+6.times do |n|
+  Blog.create!(
+    content: "anxiety#{n + 1}",
+    user_id: n + 1
+  )
+end
+
+6.times do |n|
+  Comment.create!(
+    content: "comment#{n + 1}",
+    user_id: n + 1,
+    blog_id: n + 1
+  )
+end
